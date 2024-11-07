22ea9b25ae0b4cb2d4f7538a4f5fd219fe441fa8
Hassan Wael <aqilanhassan@gmail.com>
2024-11-08 02:20:14 +0300

    Change Scope from Singlton(default) to Prototype

--- src/main/java/com/secondclass/second/FunDemoController.java
+++ src/main/java/com/secondclass/second/FunDemoController.java
@@ -10,18 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunDemoController {
 
     private Couch myCouch;
-
-//    public FunDemoController(Couch myCouch){
-//        this.myCouch = myCouch;
-//    }
+    private Couch anotherCouch;
 
     @Autowired
+    public FunDemoController(@Qualifier("ronaldo") Couch myCouch, @Qualifier("ronaldo") Couch anotherCouch){
+
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        this.myCouch = myCouch;
+        this.anotherCouch = anotherCouch;
+
+    }
+
+//    @Autowired autowired call the method auto
     public void setMyCouch(Couch myCouch){
         this.myCouch = myCouch;
     }
     @GetMapping("/get")
     public String getCouch(){
         return this.myCouch.getCouch();
+    }
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing couch with another = " + (myCouch == anotherCouch);
     }
 
 }
--- src/main/java/com/secondclass/second/couches/Ronaldo.java
+++ src/main/java/com/secondclass/second/couches/Ronaldo.java
@@ -1,9 +1,12 @@
 package com.secondclass.second.couches;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Ronaldo implements Couch{
 
     @Override
